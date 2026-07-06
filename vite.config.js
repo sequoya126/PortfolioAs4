ad5c88d812cf898302ee7ddc09a1c2177d3af04f
Sequoya Maclean-Courville <sequoya.macleanc@gmail.com>
2026-07-05 20:35:06 -0400

    fixed the branch switching

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioAs4/', // ← replace with your repo name exactly
+  base: '/PortfolioAs4/',
 });
