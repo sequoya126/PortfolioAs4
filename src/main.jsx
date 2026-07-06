87b1349d90733718f4614bcaf98f55cfcf1d7de8
Sequoya Maclean-Courville <sequoya.macleanc@gmail.com>
2026-07-05 21:44:39 -0400

    still trying to fix the reloading issue - updated main.jsx to only use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,16 @@
-// src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.jsx';
 import App from './App.jsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/PortfolioAs4">  {/* ← Add this line */}
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
 );
